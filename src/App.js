8dad0617357ef548eb99c108412d683114acb3da
Kiran K K <kirankalak4@gmail.com>
2021-05-24 07:59:29 -0700

    Started netflix clone project to study React js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import './App.css';
+import Banner from './components/Banner/Banner';
+import RowPost from './components/RowPost/RowPost';
+
+
+
+function App() {
+  return(
+    <div className="app">
+      <NavBar/>
+      <Banner/>
+      <RowPost/>
+    </div>
+  )
+}
+
+
+export default App;
